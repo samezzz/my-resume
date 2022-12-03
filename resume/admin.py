2181a574ccd85dd779f6c0d3b93abdf01e79a028
samezzz <95895530+samezzz@users.noreply.github.com>
2022-12-03 04:11:54 +0000

    environment variables

--- resume/admin.py
+++ resume/admin.py
@@ -6,7 +6,7 @@ admin.site.site_header = "My Resume"
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone_number', 'description', 'created_at')
     search_fields = ('name', 'email', 'phone_number', 'description')
-    list_per_page = 6
+    list_per_page = 10
 
 # Register your models here.
 admin.site.register(Message, MessageAdmin)
